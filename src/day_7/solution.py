106e6592d1f0348e6b78b0117c871d3e6351ebe9
Armand Adroher <aadroher@gmail.com>
2020-12-10 01:07:25 +0100

    Formats and refactors code.

--- src/day_7/solution.py
+++ src/day_7/solution.py
@@ -12,47 +12,58 @@ file_lines = [
 
 
 def parse_content(contained_bag_str):
-  number, *colour_tokens = contained_bag_str.strip().split(' ')[:3]
-  return {
-      'number': number,
-      'colour': "_".join(colour_tokens)
-  }
+    number, *colour_tokens = contained_bag_str.strip().split(' ')[:3]
+    return {
+        'number': number,
+        'colour': "_".join(colour_tokens)
+    }
 
 
 def parse_rule(rule_str):
-  container_statement, content_statement = (
-      token.strip()
-      for token
-      in rule_str.split('contain')
-  )
-  container_colour = "_".join(container_statement.split()[:2])
-  contents = [
-      parse_content(contained_bag_str)
-      for contained_bag_str
-      in content_statement.split(',')
-  ]
-  return {
-      'colour': container_colour,
-      'contents': contents
-  }
+    container_statement, content_statement = (
+        token.strip()
+        for token
+        in rule_str.split('contain')
+    )
+    container_colour = "_".join(container_statement.split()[:2])
+    contents = [
+        parse_content(contained_bag_str)
+        for contained_bag_str
+        in content_statement.split(',')
+    ]
+    return {
+        'colour': container_colour,
+        'contents': contents
+    }
 
 
-def add_rule(result, rule):
-  parent_colour = rule['colour']
-  for child in rule['contents']:
-    child_colour = child['colour']
-    parent_colour_set = result.get(child_colour, set())
-    new_parent_colour_set = {*parent_colour_set, parent_colour}
-    result[child_colour] = new_parent_colour_set
-  return result
+def update_parent_set(parent_colour, child_to_parent_colours, child_colour):
+    parent_colour_set = child_to_parent_colours.get(child_colour, set())
+    return {
+        **child_to_parent_colours,
+        child_colour: {*parent_colour_set, parent_colour}
+    }
+
+
+def add_rule(child_to_parent_colours, rule):
+    parent_colour = rule['colour']
+    return reduce(
+        lambda updated_child_to_parent_colours, child: update_parent_set(
+            parent_colour,
+            updated_child_to_parent_colours,
+            child['colour']
+        ),
+        rule['contents'],
+        child_to_parent_colours
+    )
 
 
 def get_parents_by_child(rules):
-  return reduce(
-      add_rule,
-      rules,
-      {}
-  )
+    return reduce(
+        add_rule,
+        rules,
+        {}
+    )
 
 
 rules = [
