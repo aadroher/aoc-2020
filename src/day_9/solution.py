1f92d452b8f4923f7a11898c8c75957fc3dcd006
Armand Adroher <aadroher@gmail.com>
2020-12-13 17:20:08 +0100

    Gets solution to second puzzle.

--- src/day_9/solution.py
+++ src/day_9/solution.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from pprint import pprint as pp
-from itertools import product
+from itertools import product, combinations
 from functools import reduce, cache
 from collections import namedtuple
 
@@ -15,8 +15,8 @@ numbers = [
     in file_handler.readlines()
 ]
 
-indexes = list(range(0, len(numbers) + 1))
-pp(indexes)
+indexes = list(range(0, len(numbers)))
+
 
 def is_valid(index, number):
     start = index - PREFIX_LENGTH
@@ -36,22 +36,38 @@ first_invalid_number = next(
     number
     for i, number
     in enumerate(numbers)
-    if i >= PREFIX_LENGTH and not is_valid(i, number)
+    if i >= PREFIX_LENGTH and (not is_valid(i, number))
 )
+
+# intervals = (
+#     (start, end)
+#     for start, end
+#     in product(indexes, indexes)
+#     if start + 3 < end
+# )
 
 intervals = (
     (start, end)
     for start, end
-    in product(indexes, indexes)
+    in combinations(indexes, 2)
     if start + 3 < end
 )
 
 encryption_weakness = next(
-    numbers[start] + numbers[end - 1]
+    (
+          (start, end),
+          (numbers[start], numbers[end]),
+          min(numbers[start:(end + 1)]) + max(numbers[start:(end + 1)])
+    )
     for start, end
     in intervals
-    if sum(numbers[start:end]) == first_invalid_number
+    if sum(numbers[start:(end + 1)]) == first_invalid_number
 )
+
+pp(list(intervals)[50:60])
+
+pp(numbers[464:481])
+pp(sum(numbers[464:481]))
 
 pp(f"Puzzle: {first_invalid_number}")
 
