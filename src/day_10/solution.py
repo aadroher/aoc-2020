cae1188d5639809162830774d430d574b5bbc994
Armand Adroher <aadroher@gmail.com>
2020-12-13 19:17:32 +0100

    Refactors code.

--- src/day_10/solution.py
+++ src/day_10/solution.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from pprint import pprint as pp
+from itertools import chain
 from functools import reduce
 
 allowed_jolt_diff_range = range(1, 4)
@@ -16,36 +17,51 @@ joltages = [
 outlet_joltage = 0
 device_joltage = max(joltages) + 3
 
-sorted_joltages = [
-    *sorted(joltages),
-    device_joltage
-]
 
-connections = [
-    (
-        sorted_joltages[i-1] if i > 0 else 0,
-        joltage
+def get_connections(adapter_joltages):
+    with_initial_value = chain(
+        [outlet_joltage],
+        adapter_joltages
     )
-    for i, joltage
-    in enumerate(sorted_joltages)
+    with_end_value = chain(
+        adapter_joltages,
+        [device_joltage]
+    )
+    return zip(
+        with_initial_value,
+        with_end_value
+    )
+
+
+def is_valid_chain(connections):
+    return all(
+        y - x in allowed_jolt_diff_range
+        for x, y in connections
+    )
+
+
+sorted_joltages = sorted(joltages)
+
+all_adapters_diffs = [
+    y - x for x, y
+    in get_connections(
+        sorted_joltages
+    )
 ]
 
-diffs = [
-    y - x for x, y in connections
-]
-
-diff_counts = [
+all_adapters_diff_counts = [
     (
         allowed_diff,
         sum(
             1 if diff == allowed_diff else 0
-            for diff in diffs
+            for diff in all_adapters_diffs
         )
     )
     for allowed_diff
     in allowed_jolt_diff_range
 ]
 
-product_of_1_and_3_diff_counts = diff_counts[0][1] * diff_counts[2][1]
+product_of_1_and_3_diff_counts = \
+    all_adapters_diff_counts[0][1] * all_adapters_diff_counts[2][1]
 
 pp(f"Puzzle 1: {product_of_1_and_3_diff_counts}")
