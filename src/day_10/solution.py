ddadd9f499cf13f06d23f7b227093ad9f5923a38
Armand Adroher <aadroher@gmail.com>
2020-12-16 18:33:22 +0100

    Finally gets a correct solution.

--- src/day_10/solution.py
+++ src/day_10/solution.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from pprint import pprint as pp
 from itertools import chain, combinations, product, takewhile, islice
-from functools import reduce
+from functools import reduce, cache
 from math import ceil
 from collections import namedtuple
 from multiprocessing import Pool
@@ -11,7 +11,7 @@ Adapter = namedtuple('Adapter', ['name', 'joltage', 'diff'])
 allowed_jolt_diff_range = {*range(1, 4)}
 
 current_dir = Path(__file__).parent
-file_handler = open(current_dir/"test_1.txt", 'r')
+file_handler = open(current_dir/"input.txt", 'r')
 
 joltages = [
     int(line.strip())
@@ -48,35 +48,99 @@ def get_total_adapter_chain(sorted_joltages):
     ]
 
 
-def get_path_to_end(current, steps):
-    # pp((current, steps))
-    if current == 0:
+def get_diffs(sorted_joltages):
+    return (
+        ((n, m), m - n)
+        for n, m
+        in zip(
+            chain([0], sorted_joltages),
+            sorted_joltages
+        )
+    )
+
+
+def get_one_step_intervals(sorted_diffs):
+  intervals = [[]]
+  for (_, m), diff in sorted_diffs:
+    if diff == 1:
+      intervals[-1].append(m)
+    else:
+      intervals.append([])
+  return intervals
+
+
+def get_path_to_end(current, end, steps):
+    if current == end:
         return 1
     else:
-        valid_next_steps = {
+        valid_next_steps = tuple(
             step
             for step
             in steps
             if current - step in allowed_jolt_diff_range
-        }
+        )
         if len(valid_next_steps) == 0:
             return 0
         else:
             return sum(
                 get_path_to_end(
                     next_step,
-                    steps - {next_step},
+                    end,
+                    tuple(
+                        filter(
+                            lambda step: step != next_step,
+                            steps
+                        )
+                    )
                 )
                 for next_step
                 in valid_next_steps
             )
 
 
+def add_num_paths(one_step_intervals):
+  return (
+      (
+          interval,
+          get_path_to_end(
+              max(interval),
+              min(interval) - 1,
+              list(reversed(interval)) + [min(interval) - 1]
+          )
+      )
+      for interval
+      in one_step_intervals
+      if len(interval) > 0
+  )
+
+
+def get_num_paths(with_num_paths):
+  return reduce(
+      lambda acc, one_step_interval: acc * one_step_interval[-1],
+      with_num_paths,
+      1
+  )
+
 sorted_joltages = list(sorted(joltages))
 
 total_adapter_chain = get_total_adapter_chain(
     sorted_joltages
 )
+
+pp(sorted_joltages)
+
+diffs = list(get_diffs(sorted_joltages))
+pp(diffs)
+
+one_step_intervals = list(get_one_step_intervals(diffs))
+pp(one_step_intervals)
+
+with_num_paths = list(add_num_paths(one_step_intervals))
+pp(with_num_paths)
+
+num_paths = get_num_paths(with_num_paths)
+pp(num_paths)
+
 
 all_adapters_diffs = [
     connection.diff
@@ -101,14 +165,15 @@ product_of_1_and_3_diff_counts = \
 
 pp(f"Puzzle 1: {product_of_1_and_3_diff_counts}")
 
-# reversed_joltages = list(reversed(sorted_joltages)) + [0]
-joltages_set = {*joltages, 0}
-max_joltage = max(joltages_set)
-rest = joltages_set - {max_joltage}
+reversed_joltages = tuple(reversed(sorted_joltages)) + (0,)
+# joltages_set = {*joltages, 0}
+# max_joltage = max(joltages_set)
+# rest = joltages_set - {max_joltage}
 # pp((max_joltage, rest))
-pp(
-    get_path_to_end(
-        max_joltage,
-        rest
-    )
-)
+
+# num_combinations = get_path_to_end(
+#     reversed_joltages[0],
+#     reversed_joltages[1:]
+# )
+
+# pp(f"Puzzle 2: {num_combinations}")
