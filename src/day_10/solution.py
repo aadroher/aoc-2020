a2fa0167ca91a489d9b24993f54209abfba366d8
Armand Adroher <aadroher@gmail.com>
2020-12-16 02:03:08 +0100

    WIP

--- src/day_10/solution.py
+++ src/day_10/solution.py
@@ -4,6 +4,7 @@ from itertools import chain, combinations, product, takewhile, islice
 from functools import reduce
 from math import ceil
 from collections import namedtuple
+from multiprocessing import Pool
 
 Adapter = namedtuple('Adapter', ['name', 'joltage', 'diff'])
 
@@ -85,34 +86,34 @@ def get_subpaths(connections):
 
 sorted_joltages = list(sorted(joltages))
 
-total_adapter_chain = get_total_adapter_chain(
-    sorted_joltages
-)
+# total_adapter_chain = get_total_adapter_chain(
+#     sorted_joltages
+# )
 
-pp(sorted_joltages)
+# pp(sorted_joltages)
 
-all_adapters_diffs = [
-    connection.diff
-    for connection
-    in total_adapter_chain
-]
+# all_adapters_diffs = [
+#     connection.diff
+#     for connection
+#     in total_adapter_chain
+# ]
 
-all_adapters_diff_counts = [
-    (
-        allowed_diff,
-        sum(
-            1 if diff == allowed_diff else 0
-            for diff in all_adapters_diffs
-        )
-    )
-    for allowed_diff
-    in allowed_jolt_diff_range
-]
+# all_adapters_diff_counts = [
+#     (
+#         allowed_diff,
+#         sum(
+#             1 if diff == allowed_diff else 0
+#             for diff in all_adapters_diffs
+#         )
+#     )
+#     for allowed_diff
+#     in allowed_jolt_diff_range
+# ]
 
-product_of_1_and_3_diff_counts = \
-    all_adapters_diff_counts[0][1] * all_adapters_diff_counts[2][1]
+# product_of_1_and_3_diff_counts = \
+#     all_adapters_diff_counts[0][1] * all_adapters_diff_counts[2][1]
 
-pp(f"Puzzle 1: {product_of_1_and_3_diff_counts}")
+# pp(f"Puzzle 1: {product_of_1_and_3_diff_counts}")
 
 
 def valid_steps(combination):
@@ -123,28 +124,56 @@ def valid_steps(combination):
         in enumerate(combination)
     )
 
+
 def valid_limits(combination):
-  bottom = combination[0]
-  top = combination[-1]
-  return bottom in {1, 2, 3} and top == max(joltages) 
+    bottom = combination[0]
+    top = combination[-1]
+    return bottom in {1, 2, 3} and top == max(joltages)
 
 
 def is_valid_combination(combination):
     return valid_limits(combination) and valid_steps(combination)
 
-num_joltages = len(joltages)
-total_combinations = [
-    combination
-    for r in range(
-        ceil(num_joltages / 3),
-        num_joltages + 1
-    )
-    for combination
-    in combinations(sorted_joltages, r)
-    if is_valid_combination(combination)
-]
 
-pp(len(total_combinations))
+num_joltages = len(joltages)
+
+
+def num_combinations_of_length(r):
+    pp(f"Counting combinations of length {r}")
+    return sum(
+        1 for combination
+        in combinations(sorted_joltages, r)
+        if is_valid_combination(combination)
+    )
+
+
+if __name__ == '__main__':
+      pool_size = 10
+      with Pool(pool_size) as p:
+          total = sum(
+              p.map(
+                  num_combinations_of_length,
+                  range(
+                      ceil(num_joltages / 3),
+                      num_joltages + 1
+                  )
+              )
+          )
+          pp(total)
+
+
+# total_combinations = [
+#     combination
+    # for r in range(
+    #     ceil(num_joltages / 3),
+    #     num_joltages + 1
+    # )
+#     for combination
+#     in combinations(sorted_joltages, r)
+#     if is_valid_combination(combination)
+# ]
+
+# pp(len(total_combinations))
 
 
 # connections = list(get_connections(sorted_joltages))
