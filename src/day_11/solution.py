e77d6ec2ea0c7c2066348fe70dd88255f8397f26
Armand Adroher <aadroher@gmail.com>
2020-12-17 12:00:43 +0100

    WIP

--- src/day_11/solution.py
+++ src/day_11/solution.py
@@ -17,26 +17,53 @@ seat_map = tuple(
 )
 
 
-def get_num_adjacent_states(state, pos, seat_map):
-  x, y = pos
-  translations_vector = (-1, 0, 1)
-  adjacency_translations = (
-      *product(translations_vector, translations_vector),
-  )
-  return sum(
+def is_of_state(states, pos, seat_map):
+    x, y = pos
+    return x in range(len(seat_map)) \
+        and y in range(len(seat_map[x])) \
+        and seat_map[x][y] in states
 
-  )
+
+def get_num_adjacent_states(states, pos, seat_map):
+    x, y = pos
+    translations_vector = (-1, 0, 1)
+    adjacency_translations = (
+        translation for translation
+        in product(translations_vector, translations_vector)
+        if translation != (0, 0)
+    )
+    return sum(
+        1 if is_of_state(states, (x + dx, y + dy), seat_map) else 0
+        for dx, dy in adjacency_translations
+    )
 
 
 def next_state(pos, seat_map):
-  x, y = pos
+    x, y = pos
+    current_state = seat_map[x][y]
+    if current_state == EMPTY_SEAT:
+        return OCCUPIED_SEAT \
+            if get_num_adjacent_states(OCCUPIED_SEAT, pos, seat_map) == 0 else EMPTY_SEAT
+    elif current_state == OCCUPIED_SEAT:
+        return EMPTY_SEAT \
+            if get_num_adjacent_states(OCCUPIED_SEAT, pos, seat_map) >= 4 else OCCUPIED_SEAT
+    else:
+        return current_state
 
 
 def next_tick(seat_map):
-  pass
+    return tuple(
+        tuple(
+            next_state((i, j), seat_map)
+            for j in range(len(seat_map[i]))
+        ) for i in range(len(seat_map))
+    )
 
 
 pp(seat_map)
+pp(
+    next_tick(seat_map)
+)
 
 translations_vector = (-1, 0, 1)
 adjacency_translations = (*product(translations_vector, translations_vector), )
